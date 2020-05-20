0e5667d0595ff0598ae8a2752ca03600c95f6cf6
lilang <602462536@qq.com>
2020-05-20 15:57:01 +0800

    路由封装

---
+++ src/pages/cart.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      cart
+      <order-header></order-header>
+  </div>
+</template>
+
+<script>
+import OrderHeader from "./../components/OrderHeader"
+export default {
+    name:'cart',
+    components:{
+      OrderHeader
+    }
+}
+</script>
---
+++ src/pages/order.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    home
+      <order-header></order-header>
+      <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import OrderHeader from "./../components/OrderHeader"
+export default {
+    name:'order',
+    components:{
+      OrderHeader
+    }
+}
+</script>
