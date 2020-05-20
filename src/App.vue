fcfc89f58946116674b68bdc3a5d8040e31d0c48
lilang <602462536@qq.com>
2020-05-20 17:30:59 +0800

    环境变量接口地址

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,16 @@
 </template>
 
 <script>
+import storage from "./storage/index";
 export default {
   name: 'App',
   components: {
+  },
+  mounted(){
+    storage.setItem('a',1);
+    // storage.setItem('user',{a:1})
+    // storage.setItem('abc',{a:1},'user');
+    // storage.clear('mall')
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,32 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
+import env from './env'//依赖不需要加./  文件导入需要加./
 
+// 根据前端的跨域方式做调整
+// axios.defaults.baseURL='/api';
+// 请求超时时间
+axios.defaults.timeout=8000;
+// 根据环境变量请求不同的接口地址
+axios.defaults.baseURL=env.baseURL;
+// 接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res=response.data;
+  if(res.status==0){
+    // 成功
+    return res.data;
+  }else if(res.status==10){
+    // 10 未登录
+    window.location.href='/#/login';
+  }else{
+    // 错误信息
+    alert(res.msg);
+  }
+});
+
+Vue.use(VueAxios,axios);
 Vue.config.productionTip = false
 
 new Vue({
